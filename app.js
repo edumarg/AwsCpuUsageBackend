f3757f55399e06ebf13f14c172aa04693453dda5
Eduardo Margulis <edumarg@gmx.com>
2021-12-08 10:27:41 +0200

    configure use of enviromental variables for secrets

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const express = require("express");
+const config = require("config");
 const app = express();
+
+if (
+  !config.get("accessID") ||
+  !config.get("accessKey") ||
+  !config.get("region")
+) {
+  console.log(
+    "FATAL ERROR: Either accessID, accessKey or region are not defined.."
+  );
+  process.exit(1);
+}
+
+const accessID = config.get("accessID");
+const accessKey = config.get("accessKey");
+const region = config.get("region");
 
 const port = process.env.PORT || 3000;
 app.listen(3000, () => console.log(`Listening on port ${port}...`));
